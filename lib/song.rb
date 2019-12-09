bbe44ec8d8bf4d79aa244216670c093faa125dd4
Eric Manning <emanningmusic@yahoo.com>
2019-12-09 20:09:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,13 +6,20 @@ class Song
   def self.all
     @@all
   end
-
+  
+  def initialize(name)
+    @name = name
+  end
+  
 def self.create
   s = self.new 
   s.save 
   s 
 end
 
+  def self.new_by_name
+    
+  end
 
   def save
     self.class.all << self
